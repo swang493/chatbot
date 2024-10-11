a6528b7063ae90ec61835101994a708ee8f3b333
Charan Kumar <charan2598@gmail.com>
2024-10-11 12:44:12 -0700

    Added LLM logic

---
+++ langchain_internal_policy.py
@@ -0,0 +1,42 @@
+from langchain.prompts.prompt import PromptTemplate
+from langchain_openai import ChatOpenAI
+import os
+from pypdf import PdfReader
+from dotenv import load_dotenv
+
+def fetch_policy_docs_info(policy_dir):
+    policy_docs = os.listdir(policy_dir)
+    doc_info = ""
+    for doc in policy_docs:
+        reader = PdfReader(os.path.join(policy_dir, doc))
+        doc_info = doc_info + doc + " contains the following content "
+        for page in reader.pages[:21]:
+            doc_info += page.extract_text()
+    return doc_info
+
+
+if __name__ == "__main__":
+    # Read the open_API_key
+    dotenv_path = os.path.join(os.path.dirname(__file__), '.env')
+    load_dotenv(dotenv_path)
+
+    policy_dir = r"D:\Learning\LangChain\LangChain101\policy_docs"
+    doc_info = fetch_policy_docs_info(policy_dir)
+
+    summary_template = """
+        given the document content "{information}" from coastal bank employee handbook, I want you to answer the following question:
+        1. Tell me what is the procedure when someone is harassed in three points.
+    """
+
+    summary_prompt_template = PromptTemplate(
+        input_variables="information", template=summary_template
+    )
+
+    llm = ChatOpenAI(temperature=0, model_name="gpt-3.5-turbo")
+    # Temperature parameter is a measure of how creative the model.
+
+    chain = summary_prompt_template | llm
+
+    result = chain.invoke(input={"information": doc_info})
+
+    print(result)
